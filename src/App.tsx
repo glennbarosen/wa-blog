eb9679e5dfba91238d367a2d46574dc6d3f21acf
GlennBarosen <glennbarosen@gmail.com>
2020-08-30 02:03:10 +0200

    firestore working 🏄🏻‍♂️

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from './firebase'
+import { Posts } from './Types';
+import Header from './components/Header';
 
-function App() {
+const dateToDB = Date.now()
+
+// firebase.firestore().collection('posts').add({
+//   title: 'Test',
+//   date: dateToDB,
+//   content: 'This is a test post!'
+// })
+
+// const usePosts = () => {
+//   const [posts, setPosts] = useState([] as Posts)
+
+//   useEffect(() => {
+//     firebase
+//       .firestore()
+//       .collection('posts')
+//       .onSnapshot((snapshot) => {
+//         const newPosts = snapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           ...doc.data()
+//         }))
+//         setPosts(newPosts)
+//       })
+//   }, [])
+//   return posts
+// }
+
+const App = () => {
+  // const posts = usePosts()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Header />
+    // <div className="App">
+    //   <ul>
+    //     {posts.map((post) => (
+    //       <li key={post.id}>
+    //         {post.title}<br />
+    //         {new Date(post.date).toLocaleDateString()}<br />
+    //         {post.content}<br />
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+  )
 }
 
 export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAnwNGWQtNYapOIKdnKycPQW9UbBbqVVzA',
+    authDomain: 'firestore-blog-2cea2.firebaseapp.com',
+    databaseURL: 'https://firestore-blog-2cea2.firebaseio.com',
+    projectId: 'firestore-blog-2cea2',
+    storageBucket: 'firestore-blog-2cea2.appspot.com',
+    messagingSenderId: '9465825417',
+    appId: '1:9465825417:web:2d88fabb4dd12a82089e58'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
